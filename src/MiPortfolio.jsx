e691b7a501c5325e9580abeb54d0fb3264b3742c
yasmine <adianiepene@yahoo.fr>
2026-03-13 00:57:08 +0100

    fix eslint

--- src/MiPortfolio.jsx
+++ src/MiPortfolio.jsx
@@ -214,6 +214,7 @@ const EDUCATION = [
 
 function useInView(ref) {
   const [v, setV] = useState(false);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     const o = new IntersectionObserver(([e]) => { if (e.isIntersecting) setV(true); }, { threshold: 0.1 });
     if (ref.current) o.observe(ref.current);
